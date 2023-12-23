05943025b688b8916549b8eb45a740d458d36330
TerraMonster5 <william.dargan@gmail.com>
2023-12-23 23:06:15 +0000

    Fixed world menu

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pathlib
 import json
+import functools
 
 import tkinter as tk
 from tkinter import ttk
@@ -31,21 +32,22 @@ class MainMenu(State):
 
         for i, path in enumerate(worlds):
             with open(f"adventures/{path}.json", "r") as file: name = json.load(file)["name"]
-            self.__worldBtns.append(ttk.Button(self._frame, text=name, command=lambda:self.__switchWorldMenu(f"{path}.json")))
+            self.__worldBtns.append(ttk.Button(self._frame, text=name, command=functools.partial(self.__switchAdventureMenu, f"{path}.json")))
             self.__worldBtns[i].grid(column=0, row=i+1)
 
         self.__quitBtn = ttk.Button(self._frame, text="Quit", command=root.destroy)
         self.__quitBtn.grid(column=0, row=len(self.__worldBtns)+1)
     
-    def __switchWorldMenu(self, filename):
+    def __switchAdventureMenu(self, filename):
+        print(filename)
         self._frame.destroy()
-        root.currentState = WorldMenu(filename)
+        root.currentState = AdventureMenu(filename)
 
-class WorldMenu(State):
+class AdventureMenu(State):
     def __init__(self, filename):
         super().__init__()
 
-        self.filename = filename
+        self._filename = filename
 
         self.__levelBtns = []
 
@@ -59,7 +61,7 @@ class WorldMenu(State):
             jsondict.pop("zombieIDs")
         
         for i, world in enumerate(jsondict.values()):
-            self.__levelBtns.append(ttk.Button(self._frame, text=world["name"]))
+            self.__levelBtns.append(ttk.Button(self._frame, text=world["name"], command=functools.partial(self.__switchWorldMenu, world)))
             self.__levelBtns[i].grid(column=0, row=i+1)
 
         self.__backBtn = ttk.Button(self._frame, text="Back", command=self.__switchMain)
@@ -68,16 +70,33 @@ class WorldMenu(State):
     def __switchMain(self):
         self._frame.destroy()
         root.currentState = MainMenu()
+    
+    def __switchWorldMenu(self, world):
+        self._frame.destroy()
+        root.currentState = WorldMenu(world, self._filename)
 
-class LevelMenu(State):
-    def __init__(self, filename):
+class WorldMenu(State):
+    def __init__(self, world, filename):
         super().__init__()
 
-        self.filename = filename
+        self._filename = filename
+
+        self.__title = ttk.Label(self._frame, text=world["name"])
+        self.__title.grid(column=0, row=0)
+
+        self.__backBtn = ttk.Button(self._frame, text="Back", command=self.__switchBack)
+        self.__backBtn.grid(column=0, row=1)
     
     def __switchBack(self):
         self._frame.destroy()
-        root.currentState = WorldMenu(self.filename)
+        root.currentState = AdventureMenu(self._filename)
+
+class LevelMenu(State):
+    def __init__(self, level, world, filename):
+        super().__init__()
+
+        self._filename = filename
+        self._world = world
 
 root = Main()
 root.currentState = MainMenu()
