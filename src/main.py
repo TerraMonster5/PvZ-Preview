4eba6ba019b2815c47668b7b81720c57bc0b6a8b
TerraMonster5 <william.dargan@gmail.com>
2024-04-23 22:33:22 +0100

    Added test zombie list

--- src/main.py
+++ src/main.py
@@ -19,19 +19,19 @@ class Main(tk.Tk):
         self.geometry("600x600")
         self.title("PvZ Preview")
 
-        self.toolbar = stk.ToolBar(self, tearoff=0)
-        self.toolbar.add_cascade("file", menukw={"tearoff": 0}, cascadekw={"label": "File"})
-        self.toolbar.file.add_command(label="Open", command=lambda: ...) # type: ignore
-        self.toolbar.file.add_command(label="Open recent", command=lambda: ...) # type: ignore
-        self.toolbar.file.add_separator() # type: ignore
-        self.toolbar.file.add_command(label="Save as", command=lambda: ...) # type: ignore
-        self.toolbar.file.add_separator() # type: ignore
-        self.toolbar.file.add_command(label="Close", command=lambda: ...) # type: ignore
-        self.toolbar.file.add_command(label="Exit", command=lambda: exit()) # type: ignore
-        self.toolbar.add_cascade("tools", menukw={"tearoff": 0}, cascadekw={"label": "Tools"})
-        self.toolbar.tools.add_cascade("calculators", menukw={"tearoff": 0}, cascadekw={"label": "Calculators"}) # type: ignore
-        self.toolbar.tools.calculators.add_command(label="50% Rule", command=lambda: FiftyPercent()) # type: ignore
-        self.config(menu=self.toolbar)
+        self.__toolbar = stk.ToolBar(self, tearoff=0)
+        self.__toolbar.add_cascade("file", menukw={"tearoff": 0}, cascadekw={"label": "File"})
+        self.__toolbar.file.add_command(label="Open", command=lambda: ...) # type: ignore
+        self.__toolbar.file.add_command(label="Open recent", command=lambda: ...) # type: ignore
+        self.__toolbar.file.add_separator() # type: ignore
+        self.__toolbar.file.add_command(label="Save as", command=lambda: ...) # type: ignore
+        self.__toolbar.file.add_separator() # type: ignore
+        self.__toolbar.file.add_command(label="Close", command=lambda: ...) # type: ignore
+        self.__toolbar.file.add_command(label="Exit", command=lambda: exit()) # type: ignore
+        self.__toolbar.add_cascade("tools", menukw={"tearoff": 0}, cascadekw={"label": "Tools"})
+        self.__toolbar.tools.add_cascade("calculators", menukw={"tearoff": 0}, cascadekw={"label": "Calculators"}) # type: ignore
+        self.__toolbar.tools.calculators.add_command(label="50% Rule", command=lambda: FiftyPercent()) # type: ignore
+        self.config(menu=self.__toolbar)
 
 class State:
     def __init__(self):
@@ -143,10 +143,16 @@ class FiftyPercent(tk.Toplevel):
         self.title("50% Rule")
 
         with open("zombieHealth.json", "r") as file:
-            jsonDict = json.load(file)
+            jsondict = json.load(file)
 
-        self.zombiesDropDown = ttk.Combobox(self, state="readonly", values=list(jsonDict.keys()))
-        self.zombiesDropDown.grid(row=0, column=0)
+        ttk.Label(self, text="Zombies").grid(row=0, column=0)
+
+        self.__zombiesDropDown = ttk.Combobox(self, state="readonly", values=list(jsondict.keys()))
+        self.__zombiesList = tk.LabelFrame(self, labelwidget=self.__zombiesDropDown)
+        self.__zombiesList.grid(row=1, column=0)
+
+        for x in range(10):
+            ttk.Label(self.__zombiesList, text=f"Test {x}").grid(row=x, column=0)
 
         self.mainloop()
 
