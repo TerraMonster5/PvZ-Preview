5e47757159a724abc61e8131ba8116e17732213f
TerraMonster5 <william.dargan@gmail.com>
2024-05-01 22:02:37 +0100

    Added toolbar to calculator

--- src/main.py
+++ src/main.py
@@ -32,6 +32,8 @@ class Main(tk.Tk):
         self.__toolbar.add_cascade("tools", menukw={"tearoff": 0}, cascadekw={"label": "Tools"})
         self.__toolbar.tools.add_cascade("calculators", menukw={"tearoff": 0}, cascadekw={"label": "Calculators"}) # type: ignore
         self.__toolbar.tools.calculators.add_command(label="50% Rule", command=lambda: FiftyPercent()) # type: ignore
+        self.__toolbar.add_cascade("help", menukw={"tearoff": 0}, cascadekw={"label": "Help"})
+        self.__toolbar.help.add_command(label="About", command=lambda: ...) # type: ignore
         self.config(menu=self.__toolbar)
 
 class State:
@@ -143,6 +145,12 @@ class FiftyPercent(tk.Toplevel):
         self.iconbitmap("icon.ico")
         self.title("50% Rule")
         self.config(padx="3p", pady="3p")
+
+        self.__toolbar = stk.ToolBar(self, tearoff=0)
+        self.__toolbar.add_cascade("file", menukw={"tearoff": 0}, cascadekw={"label": "File"})
+        self.__toolbar.file.add_command(label="Close", command=lambda: self.destroy()) # type: ignore
+        self.__toolbar.file.add_command(label="Exit", command=lambda: exit()) # type: ignore
+        self.config(menu=self.__toolbar)
 
         with open("zombieHealth.json", "r") as file:
             jsondict = json.load(file)
