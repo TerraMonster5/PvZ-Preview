d8a36e0ea7061466c21e9994bf8ac86db1770b3a
TerraMonster5 <william.dargan@gmail.com>
2024-05-19 22:17:48 +0100

    Added calculator backend
    Co-authored-by: Cyber-Gon <Cyber-Gon@users.noreply.github.com>

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,5 +1,6 @@
 from . import widgets
 
+import math
 import json
 import tkinter as tk
 from tkinter import ttk
@@ -21,18 +22,59 @@ class FiftyPercent(tk.Toplevel):
         self.config(menu=self.__toolbar)
 
         with open("zombieHealth.json", "r") as file:
-            jsondict = json.load(file)
+            self.__healthDict = json.load(file)
 
         ttk.Label(self, text="Zombies").grid(row=0, column=0)
 
-        self.__selector = widgets.ZombieSelector(self, options=list(jsondict.keys()), max=8)
-        self.__selector.grid(row=1, rowspan=10, column=0, padx="2p", pady="2p", ipadx="1p", ipady="1p")
+        self.__selector = widgets.ZombieSelector(self, options=list(self.__healthDict.keys()), max=8)
+        self.__selector.grid(row=1, rowspan=10, column=0, padx="2p", pady="2p", ipadx="1p", ipady="1p", sticky=tk.N)
 
         ttk.Label(self, text="Peas").grid(row=0, column=1)
-        ttk.Entry(self).grid(row=1, column=1)
+        self.__peasDmgEntry = ttk.Entry(self)
+        self.__peasDmgEntry.grid(row=1, column=1, pady="2p")
+
         ttk.Label(self, text="True Damage").grid(row=2, column=1)
-        ttk.Entry(self).grid(row=3, column=1)
-        ttk.Button(self, text="Calculate").grid(row=4, column=1)
-        ttk.Entry(self, state="disabled").grid(row=5, column=1)
+        self.__trueDmgEntry = ttk.Entry(self)
+        self.__trueDmgEntry.grid(row=3, column=1, pady="2p")
+
+        ttk.Button(self, text="Calculate", command=lambda: self.__calculate()).grid(row=4, column=1)
+
+        ttk.Label(self, text="True Damage needed for a chance to activate 50% rule:", width=30, wraplength=184, justify="center").grid(row=5, column=1)
+        self.__lowerVar = tk.StringVar()
+        self.__lowerOutput = ttk.Entry(self, state="readonly", textvariable=self.__lowerVar)
+        self.__lowerOutput.grid(row=6, column=1, pady="2p")
+
+        ttk.Label(self, text="True Damage needed to guarantee activation of 50% rule:", width=30, wraplength=184, justify="center").grid(row=7, column=1)
+        self.__upperVar = tk.StringVar()
+        self.__upperOutput = ttk.Entry(self, state="readonly", textvariable=self.__upperVar)
+        self.__upperOutput.grid(row=8, column=1, pady="2p")
+
+        ttk.Label(self, text="Chance of 50% rule activating:", width=30).grid(row=9, column=1)
+        self.__chanceVar = tk.StringVar()
+        self.__chanceOutput = ttk.Entry(self, state="readonly", textvariable=self.__chanceVar)
+        self.__chanceOutput.grid(row=10, column=1, pady="2p")
 
         self.mainloop()
+    
+    def __calculate(self) -> None:
+        selected = self.__selector.getRecords()
+        if selected is None:
+            return
+        
+        trueDmg = int(self.__peasDmgEntry.get())*20
+        
+        #trueDmg = int(self.__trueDmgEntry.get())
+        deadHealth = sum((sum(self.__healthDict[key][x] for x in range(0, 3, 2))+self.__healthDict[key][1]*0.2)*value[1] for key, value in selected.items())
+        totalHealth = sum((sum(self.__healthDict[key][x] for x in range(0, 3, 2))+self.__healthDict[key][1]*0.2)*sum(value) for key, value in selected.items())
+
+        percentage = (trueDmg + deadHealth) / totalHealth
+
+        self.__lowerVar.set(str(math.ceil(totalHealth*0.35)))
+        self.__upperVar.set(str(math.ceil(totalHealth*0.5)))
+
+        if percentage<0.35:
+            self.__chanceVar.set("0%")
+        elif percentage>0.5:
+            self.__chanceVar.set("100%")
+        else:
+            self.__chanceVar.set(str(round(((percentage-0.35)/0.15)*100, 2))+"%")
