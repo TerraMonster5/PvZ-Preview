f6801afd76fb90116a1d5c078f7817133014540e
TerraMonster5 <william.dargan@gmail.com>
2024-05-19 19:05:10 +0100

    Fully implemented new custom widget

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -25,44 +25,14 @@ class FiftyPercent(tk.Toplevel):
 
         ttk.Label(self, text="Zombies").grid(row=0, column=0)
 
-        self.__zombiesDropDown = ttk.Combobox(self, state="readonly", values=list(jsondict.keys()), width=15)
-        self.__zombiesDropDown.bind("<<ComboboxSelected>>", self.__addZombie)
-        self.__zombiesList = tk.LabelFrame(self, labelwidget=self.__zombiesDropDown)
-        self.__zombiesList.grid(row=1, column=0, rowspan=10, padx="2p", pady="2p", ipadx="1p", ipady="1p")
+        self.__selector = widgets.ZombieSelector(self, options=list(jsondict.keys()), max=8)
+        self.__selector.grid(row=1, rowspan=10, column=0, padx="2p", pady="2p", ipadx="1p", ipady="1p")
 
-        self.__free = 0
-
-        ttk.Label(self.__zombiesList, text="Type").grid(row=0, column=0)
-        ttk.Label(self.__zombiesList, text="Alive").grid(row=0, column=1)
-        ttk.Label(self.__zombiesList, text="Dead").grid(row=0, column=2)
-
-        self.__choices = [ttk.Label(self.__zombiesList, text=f"", width=15) for _ in range(8)]
-        [x.grid(row=c+1, column=0) for c, x in enumerate(self.__choices)]
-
-        self.__numFields = [[ttk.Spinbox(self.__zombiesList, from_=0, to=99, increment=1, width=3) for _ in range(2)] for _ in range(8)]
-        [[wgt[c-1].grid(row=r+1, column=c) for c in range(1, 3)] for r, wgt in enumerate(self.__numFields)]
-
-        # ttk.Label(self, text="Peas").grid(row=0, column=1)
-        # ttk.Entry(self).grid(row=1, column=1)
-        # ttk.Label(self, text="True Damage").grid(row=2, column=1)
-        # ttk.Entry(self).grid(row=3, column=1)
-        # ttk.Button(self, text="Calculate").grid(row=4, column=1)
-        # ttk.Entry(self, state="disabled").grid(row=5, column=1)
-
-        cancelIcon = tk.PhotoImage(file="assets/close.png").subsample(4, 4)
-
-        temp = [tk.Button(self.__zombiesList, image=cancelIcon, relief="flat", overrelief="flat", bd=0, width=10) for r in range(8)]
-        [wgt.grid(row=r+1, column=3) for r, wgt in enumerate(temp)]
-
-        temp2 = widgets.ZombieSelector(self, options=list(jsondict.keys()), max=8)
-        temp2.grid(row=1, column=1, padx="2p", pady="2p", ipadx="1p", ipady="1p")
-
-        self.update()
-        print(self.__numFields[0][0].winfo_width())
+        ttk.Label(self, text="Peas").grid(row=0, column=1)
+        ttk.Entry(self).grid(row=1, column=1)
+        ttk.Label(self, text="True Damage").grid(row=2, column=1)
+        ttk.Entry(self).grid(row=3, column=1)
+        ttk.Button(self, text="Calculate").grid(row=4, column=1)
+        ttk.Entry(self, state="disabled").grid(row=5, column=1)
 
         self.mainloop()
-
-    def __addZombie(self, event):
-        if self.__free < len(self.__choices) and not self.__zombiesDropDown.get() in map(lambda x: x["text"], self.__choices):
-            self.__choices[self.__free].config(text=self.__zombiesDropDown.get())
-            self.__free += 1
--- src/tools/widgets.py
+++ src/tools/widgets.py
@@ -10,10 +10,12 @@ class ZombieSelector(tk.LabelFrame):
                  **kwargs) -> None:
         kwargs = cnf or kwargs
 
-        dropDown = ttk.Combobox(master, state="readonly", values=options, width=15)
-        dropDown.bind("<<ComboboxSelected>>", self.__addZombie)
+        self.__max = max
 
-        super().__init__(master, labelwidget=dropDown, width=184, height=max*20+42, **kwargs)
+        self.__dropDown = ttk.Combobox(master, state="readonly", values=options, width=15)
+        self.__dropDown.bind("<<ComboboxSelected>>", self.__addZombie)
+
+        super().__init__(master, labelwidget=self.__dropDown, width=184, height=max*20+42, **kwargs)
 
         self.grid_propagate(False)
         self.columnconfigure(1, minsize=37)
@@ -22,11 +24,42 @@ class ZombieSelector(tk.LabelFrame):
         ttk.Label(self, text="Type", width=15, anchor="center").grid(row=0, column=0)
         ttk.Label(self, text="Alive").grid(row=0, column=1)
         ttk.Label(self, text="Dead").grid(row=0, column=2)
+
+        self.__records = [[] for _ in range(max)]
+        self.__free = 0
+
+        self.__cancelIcon = tk.PhotoImage(file="assets/close.png").subsample(4, 4)
     
     def __addZombie(self, event) -> None:
-        pass
+        if self.__free >= self.__max:
+            return
+        
+        new = self.__dropDown.get()
 
-    def __removeZombie(self) -> None:
+        if new in map(lambda x: x["text"], [r[0] for r in self.__records if len(r) > 0]):
+            return
+        
+        self.__records[self.__free].append(ttk.Label(self, text=new))
+        for _ in range(2):
+            self.__records[self.__free].append(ttk.Spinbox(self,
+                                                         from_=0,
+                                                         to=99,
+                                                         increment=1,
+                                                         width=3))
+        self.__records[self.__free].append(tk.Button(self,
+                                                   image=self.__cancelIcon,
+                                                   relief="flat",
+                                                   overrelief="flat",
+                                                   bd=0,
+                                                   width=10))
+        for col in range(4):
+            self.__records[self.__free][col].grid(row=self.__free+1, column=col)
+
+        print(self.__records)
+
+        self.__free += 1
+
+    def __removeZombie(self, row: int) -> None:
         pass
 
     def __getRecords(self):
