35a329dd9dd9de56a5359ec85d0b98878f420054
TerraMonster5 <william.dargan@gmail.com>
2024-08-04 02:29:35 +0100

    Added binding for changing state of any checkbox

--- src/pvz-preview/states/levelMenu/widgets.py
+++ src/pvz-preview/states/levelMenu/widgets.py
@@ -23,19 +23,19 @@ class PreviewFilter(tk.LabelFrame):
         self.__groups: list[list[tuple[ttk.Checkbutton, tk.BooleanVar]]] = [[] for _ in range(len(self.__headings))]
 
         byPreviews = frame.groupby(previews)
-        groups = byPreviews.groups.keys()
+        self.__byPreviewsGroups = byPreviews.groups.keys()
 
         print(zombs, previews)
 
         options = {z: set() for z in zombs}
-        for grp in groups:
+        for grp in self.__byPreviewsGroups:
             for i in range(len(previews)):
                 options[zombs[i]].add(grp[i])
         
         for c, key in enumerate(options.keys()):
             options[key] = sorted(list(options[key])) # type: ignore
             for d, num in enumerate(options[key]):
-                self.__groups[c].append((ttk.Checkbutton(self, text=str(int(num))), tk.BooleanVar()))
+                self.__groups[c].append((ttk.Checkbutton(self, text=str(int(num)), command=self.__updateCheckbuttons), tk.BooleanVar()))
                 self.__groups[c][d][0].config(variable=self.__groups[c][d][1])
 
         for i, wgt in enumerate(self.__headings):
@@ -44,6 +44,24 @@ class PreviewFilter(tk.LabelFrame):
         for e, col in enumerate(self.__groups):
             for f, tup in enumerate(col, 1):
                 tup[0].grid(row=f, column=e)
+
+        print(self.__groups)
     
-    def __updateCheckbuttons(self, event):
-        pass
+    def __updateCheckbuttons(self):
+        for _, col in enumerate(self.__groups):
+            for _, tup in enumerate(col, 1):
+                tup[0].grid_forget()
+        
+        checked: list = [-1 for _ in range(4)]
+
+        for i, col in enumerate(self.__groups):
+            for _, tup in enumerate(col, 1):
+                if tup[1].get():
+                    checked[i] = int(tup[0].cget("text"))
+        
+        # checked = [-1, 1, -1, 2]
+        # self.__byPreviewsGroups = [[2, 1, 3, 2], [1, 2, 2, 2]]
+
+        for i, col in enumerate(self.__groups):
+            for e, tup in enumerate(col, 1):
+                pass
